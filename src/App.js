6aa491da8f5fc7cebca29c9b77cde3412e79420e
Margaux Darriberouge <65903630+Margaux-dev@users.noreply.github.com>
2020-09-18 19:09:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,65 @@ import Chat from './Chat';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
 
+import db from "./firebase";
+
 function App() {
+
+  
   // eslint-disable-next-line
   const [{ user }, dispatch] = useStateValue();
 
+  const addNewUser = () => {
+    let existingUsers = [];
+    db.collection("utilisateurs").get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+            const docData = Object.values(doc.data());
+            if (docData.some(e => e === user.uid)) {
+              return existingUsers.push(1);
+            } else {
+              return existingUsers.push(0);
+            }
+      }); 
+      if (existingUsers.some(e => e === 1)){
+        console.log("Already exists")
+        return
+      } else {
+        console.log("First connection")
+        db.collection("utilisateurs").add({
+          name: user.displayName,
+          id: user.uid,
+          photo: user.photoURL
+        })
+      }
+    }); 
+  }
+  
+  let userLocal = JSON.parse(localStorage.getItem("user"));
+
   return (
     <div className="app">
-      {!user ? 
-        ( <Login /> ) 
-        : (
+      {!userLocal ? 
+        (!user ? 
+          (<Login />) 
+          : ( 
+              <div className="app-body" onLoad={addNewUser()} >
+                <Router>
+                  <Sidebar/>
+                  <Switch>
+                    <Route path="/rooms/:roomId">
+                      <Chat />
+                    </Route>
+                    <Route path="/">
+                      <Chat />
+                    </Route>
+                  </Switch>
+                </Router>
+              </div>
+            )
+        ) : ( 
           <div className="app-body">
             <Router>
-              <Sidebar />
+              <Sidebar/>
               <Switch>
                 <Route path="/rooms/:roomId">
                   <Chat />
@@ -29,7 +76,7 @@ function App() {
             </Router>
           </div>
         )
-      } 
+        } 
     </div>
   );
 }
